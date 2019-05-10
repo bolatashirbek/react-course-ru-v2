29c94fdf24a55dd07e3fe3076f6035d9673fdcb6
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-05-10 22:33:04 +0600

    Refactoring: Made an asynchronous request for newsData.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import {Add} from "./components/Add";
 import {News} from "./components/News";
-import newsData from "./data/newsData"
 import './App.css';
 
 class App extends React.Component {
     state = {
-        news: newsData
+        news: null,
+        isLoading: false
     };
     handleAddNews = (data) => {
         const nextNews = [data, ...this.state.news];
         this.setState({news: nextNews})
     };
 
+    componentDidMount() {
+        this.setState({isLoading: true});
+        fetch('http://localhost:3000/data/newsData.json')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                //console.log('Приехали данные ', data);
+                this.setState({isLoading: false, news: data})
+            })
+    }
+
     render() {
+        const {news, isLoading} = this.state;
         return (
             <React.Fragment>
                 <Add onAddNews={this.handleAddNews}/>
                 <h3>Новости</h3>
-                <News data={this.state.news}/>
+                {isLoading && <p>Загружаю...</p>}
+                {Array.isArray(news) && <News data={news}/>}
             </React.Fragment>
         )
     }
